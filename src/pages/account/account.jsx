ac7ec592f92a3162ad6c5e163e12199a39a3b6a6
sruthijaganmohan <sruthi12jagan@gmail.com>
2023-04-30 22:37:18 +0530

    rendered account page

--- src/pages/account/account.jsx
+++ src/pages/account/account.jsx
@@ -1,5 +1,46 @@
 import React from "react";
+import './account.css'
+import lipsImage from "../../images/byte-bar-lips.png";
+import { Sparkle } from "phosphor-react";
+import Footer from "../../components/Footer";
+import { Link } from "react-router-dom";
 
 export const Account = () => {
-    return <div>Account</div>
+    return (
+    <div className="account-container">
+        <div className="account">
+        <div className="image-container">
+        <img
+            className="lips-image"
+            src={lipsImage}
+        />
+        </div>
+
+        <div className="details-container">
+        <div className="hello" id="hello">
+                <Sparkle size={48} color="#fff57f" weight="fill" style={{ marginLeft: "20px", marginRight: "20px" }}/>
+                <h1>Hello User!</h1>
+                <Sparkle size={48} color="#fff57f" weight="fill" style={{ marginLeft: "20px", marginRight: "20px" }}/>
+        </div>
+            <div className="account-shop-link">
+                <Link id="account-details-link" to="/">
+                    details
+                </Link>    
+            </div><br/><br/>
+
+            <div className="account-shop-link">
+                <Link id="account-orders-link" to="/">
+                    orders
+                </Link>    
+            </div><br/><br/>
+            
+            <div className="account-shop-link">
+                <Link id="account-shop-link" to="/shop">
+                    shop
+                </Link>    
+            </div>
+        </div>
+        </div>
+        <Footer/>
+    </div>);
 }
